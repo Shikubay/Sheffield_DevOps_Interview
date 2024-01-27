5afbab65c8611c12570cd6f962eb82f9a864dd9e
Shikubay <105959772+Shikubay@users.noreply.github.com>
2024-01-27 20:48:53 +0000

    Adds interview question file
    This is the original interview file to track changes in later commits.

---
+++ devops-engineer-task/conf.tf
@@ -0,0 +1,21 @@
+terraform {
+# backend configuration to store statefiles
+backend "s3" {
+    bucket         = "mystate-file-for-collaboration"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+}  
+  required_version = ">= 1.2.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
